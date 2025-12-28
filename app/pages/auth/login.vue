89e1f4a776849fe72089821c9907d5e838ac7c77
Donald_Edwin <donaldedu172@gmail.com>
2025-12-28 19:22:42 +0300

    the login now works

--- app/pages/auth/login.vue
+++ app/pages/auth/login.vue
@@ -1,9 +1,25 @@
 <script setup>
 import LoginForm from "@/components/login-04/components/LoginForm.vue";
-
+import { authRepository } from '~~/repository/auth'
 definePageMeta({
   layout: "auth",
 });
+
+const client = useSupabaseClient()
+const auth = authRepository(client)
+const isLoading = ref(false)
+
+const handleLogin = async (credentials) => {
+    isLoading.value = true
+    try {
+        await auth.login(credentials)
+        await navigateTo('/')
+    } catch (error) {
+        alert(error.message || "An error occurred during login")
+    } finally {
+        isLoading.value = false
+    }
+}
 </script>
 
 <script setup lang="ts"></script>
@@ -13,7 +29,7 @@ definePageMeta({
     class="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10"
   >
     <div class="w-full max-w-sm md:max-w-4xl">
-      <LoginForm />
+      <LoginForm @submit="handleLogin" :loading="isLoading"/>
     </div>
   </div>
 </template>
