c8b9d6a9b2a783df415f0015f04c12907d779ff7
Donald_Edwin <donaldedu172@gmail.com>
2025-12-28 18:07:31 +0300

    added supabase and login UI

--- app/pages/auth/registration.vue
+++ app/pages/auth/registration.vue
@@ -1,5 +1,19 @@
+<script setup>
+import SignupForm from "@/components/signup-04/components/SignupForm.vue";
+
+definePageMeta({
+  layout: "auth",
+});
+</script>
+
+<script setup lang="ts"></script>
+
 <template>
-    <div>
-        Registration
+  <div
+    class="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10"
+  >
+    <div class="w-full max-w-sm md:max-w-4xl">
+      <SignupForm />
     </div>
+  </div>
 </template>
